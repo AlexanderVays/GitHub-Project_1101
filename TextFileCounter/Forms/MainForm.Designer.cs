591e6918cddc930021b55aa9d50b6451c51a3aec
Alexander Vays <vays.alex@gmail.com>
2020-05-15 14:06:39 -0700

    added test repository

---
+++ TextFileCounter/Forms/MainForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TextFileCounter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.labelTextFile = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selectBox
+            // 
+            this.selectBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.selectBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectBox.FormattingEnabled = true;
+            this.selectBox.Items.AddRange(new object[] {
+            "Count words",
+            "Count phrases",
+            "Replace words",
+            "Replace phrases "});
+            this.selectBox.Location = new System.Drawing.Point(114, 269);
+            this.selectBox.Name = "selectBox";
+            this.selectBox.Size = new System.Drawing.Size(317, 37);
+            this.selectBox.TabIndex = 3;
+            this.selectBox.SelectedIndexChanged += new System.EventHandler(this.selectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(454, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Please choose from the following options";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(806, 70);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Welcome to a text editor application which allows the user to count and replace w" +
+    "ords and phrases";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Please the text file";
+            // 
+            // labelTextFile
+            // 
+            this.labelTextFile.AutoSize = true;
+            this.labelTextFile.Location = new System.Drawing.Point(45, 141);
+            this.labelTextFile.Name = "labelTextFile";
+            this.labelTextFile.Size = new System.Drawing.Size(0, 20);
+            this.labelTextFile.TabIndex = 8;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(49, 161);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(138, 38);
+            this.btnOpen.TabIndex = 9;
+            this.btnOpen.Text = "&Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFileName.Location = new System.Drawing.Point(204, 168);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(0, 25);
+            this.labelFileName.TabIndex = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 814);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.labelTextFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectBox);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox selectBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label labelTextFile;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label labelFileName;
+    }
+}
+
---
+++ TextFileCounter/Forms/WordsCountControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextFileCounter
+{
+    public partial class WordsCountControl : UserControl
+    {
+        public WordsCountControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            List<string> wordsList = new List<string> { textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text };
+            wordsList.RemoveAll(item => item == null || item == "");
+            bool caseSensitive = checkBoxCaseSensitive.Checked;
+            Reader.GetCount(wordsList, caseSensitive);
+            CountResultForm resultForm = new CountResultForm(1);
+            resultForm.ShowDialog();
+        }
+
+        /*private void resultForm_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Reader.resultDictionary.Clear();
+        } */
+    }
+}
