19faa402730d69f1154e1e35721bf250f0498f2c
Alexander Vays <vays.alex@gmail.com>
2020-06-02 12:54:38 -0700

    A Lazy way of Singleton Implementation

---
+++ Singleton/ISingleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Singleton
+{
+    interface ISingleton
+    {
+        string GetRead(string fileName);
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    //Singleton class is completely thread-safe. It is loaded in a "Lazy" way which means that our instance is going to be created only when it is actually needed. 
+    public sealed class Singleton
+    {
+        
+        private static Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton Instance => instance.Value;
+
+        private Dictionary<string, int> vocabulary = new Dictionary<string, int>();
+        public string fileName { get; set; }
+
+        private Singleton() 
+        { 
+        }
+
+        public Dictionary<string, int> GetRead(string fileName) 
+        {
+            return vocabulary;
+        }
+    }
+}
